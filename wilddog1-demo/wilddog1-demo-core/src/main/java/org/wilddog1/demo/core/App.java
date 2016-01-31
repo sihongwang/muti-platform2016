750734661701fecdbfb33e421c96af31f6f51f6b
wangsihong <wangsihong@able-elec.com>
2016-01-31 23:41:06 +0800

    提交

---
+++ wilddog1-demo/wilddog1-demo-core/src/main/java/org/wilddog1/demo/core/App.java
@@ -0,0 +1,13 @@
+package org.wilddog1.demo.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
